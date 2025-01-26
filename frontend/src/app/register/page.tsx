9f612e402bd8765a0fa0c3e089399f39488a8765
seyade <sey.adegun@gmail.com>
2025-01-26 18:41:02 +0000

    added image

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -3,11 +3,16 @@
 import React, { useState } from "react";
 import * as actions from "@/actions";
 import Input from "@/components/Input";
+import Link from "next/link";
+import Image from "next/image";
+
+import promoImage from "/assets/images/cup-coffee-laptop.jpg";
 
 type RegisterUser = {
   email: string;
   username: string;
   password: string;
+  confirmPassword: string;
   githubAccount?: string;
 };
 
@@ -16,6 +21,7 @@ const Register = () => {
     email: "",
     username: "",
     password: "",
+    confirmPassword: "",
     githubAccount: "",
   });
 
@@ -32,35 +38,67 @@ const Register = () => {
 
   return (
     <div className="grid grid-cols-12">
-      <header className="col-span-12 text-center">
-        <h1 className="text-3xl font-semibold">Sign up</h1>
-        <h2 className="text-md font-extralight">Create an account</h2>
-      </header>
+      <section className="col-span-12 grid grid-cols-2 gap-4  h-screen">
+        <div className="col-span-1 flex flex-col items-center w-full py-10 px-32">
+          <header className="col-span-12 mb-10 text-center">
+            <h1 className="text-3xl font-semibold">Sign up</h1>
+            <h2 className="text-md font-extralight">Create an account</h2>
+          </header>
 
-      <section className="col-span-3">
-        <form>
-          <Input
-            id="username"
-            labelText="Username"
-            onChange={handleOnChange}
-            value={formData.username}
-          />
+          <form className="w-full">
+            <Input
+              id="username"
+              placeholder="Username"
+              onChange={handleOnChange}
+              value={formData.username}
+            />
 
-          <Input
-            id="email"
-            labelText="Email"
-            onChange={handleOnChange}
-            value={formData.email}
-          />
+            <Input
+              id="email"
+              placeholder="Email"
+              onChange={handleOnChange}
+              value={formData.email}
+            />
 
-          <button
-            type="submit"
-            onClick={registerSubmit}
-            className="w-full py-2 bg-orange-400 rounded-xl"
-          >
-            Submit
-          </button>
-        </form>
+            <Input
+              id="password"
+              placeholder="Password"
+              onChange={handleOnChange}
+              value={formData.password}
+            />
+
+            <Input
+              id="confirmPassword"
+              placeholder="Confirm password"
+              onChange={handleOnChange}
+              value={formData.confirmPassword}
+            />
+
+            <button
+              type="submit"
+              onClick={registerSubmit}
+              className="w-full py-4 mt-4 bg-amber-300 rounded-xl"
+            >
+              Submit
+            </button>
+          </form>
+          <p className="py-4">
+            Already registered?{" "}
+            <Link href="/sign-in" className="text-orange-700">
+              Sign in.
+            </Link>
+          </p>
+        </div>
+        <div className="col-span-1 p-11">
+          <div className="flex justify-center items-center bg-amber-300 rounded-3xl w-full h-full">
+            <Image
+              src={promoImage}
+              width={100}
+              height={100}
+              alt="Cup of Coffe and a laptop"
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -5,26 +5,36 @@ type InputProps = {
   labelText?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
+  placeholder?: string;
   error?: string;
+  type?: string;
 };
 
-const Input = ({ id, labelText, onChange, value, error }: InputProps) => {
+const Input = ({
+  id,
+  labelText,
+  onChange,
+  value,
+  error,
+  placeholder,
+  type,
+}: InputProps) => {
   return (
-    <div className="mb-3">
+    <div className="mb-6">
       <span className="flex flex-col">
         {labelText && (
-          <label htmlFor="email" className="pl-3">
+          <label htmlFor="email" className="pl-4 mb-2 text-sm text-amber-950">
             {labelText}
           </label>
         )}
         <input
           id={id}
           name={id}
-          type="text"
+          type={type || "text"}
           onChange={onChange}
           value={value}
-          className="py-2 px-3 rounded-xl"
-          placeholder={labelText}
+          className="py-3 px-4 rounded-xl"
+          placeholder={placeholder}
         />
       </span>
       {error && <p>{error}</p>}
