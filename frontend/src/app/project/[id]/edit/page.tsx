f475831479f735f59bda2717aba8e927d46ecdfd
seyade <sey.adegun@gmail.com>
2025-01-29 21:44:57 +0000

    sidebar and save placeholders added

--- frontend/src/app/project/[id]/edit/page.tsx
+++ frontend/src/app/project/[id]/edit/page.tsx
@@ -16,21 +16,40 @@ const ProjectEditor = (props: Props) => {
     setStackedView(!stackedView);
   };
 
-  return (
-    <div className="plusjakartasans py-3 px-4">
-      <header className="flex justify-between mb-6">
-        <h1 className="spacegrotesk text-3xl font-semibold">Edit Project</h1>
-        <button
-          className="py-2 px-4 bg-slate-900 text-white text-xs rounded-full"
-          onClick={handleSwitchView}
-        >
-          Change pane View
-        </button>
-      </header>
+  const handleSave = () => {
+    // TODO: capture the code and save it
+  };
 
-      <section>
-        <Editor id="1" code="{/* write some code */}" stacked={stackedView} />
-      </section>
+  return (
+    <div className="plusjakartasans flex">
+      <aside className="bg-white p-1">Sidebar</aside>
+      <div className="w-full py-3  px-4">
+        <header className="flex justify-between mb-6">
+          <h1 className="spacegrotesk flex-1 text-3xl font-semibold">
+            Edit Project
+          </h1>
+
+          <div className="flex-1 text-right">
+            <button
+              className="py-2 px-4 mr-4 bg-green-600 text-white text-xs rounded-full"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+
+            <button
+              className="py-2 px-4 bg-slate-900 text-white text-xs rounded-full"
+              onClick={handleSwitchView}
+            >
+              Change pane View
+            </button>
+          </div>
+        </header>
+
+        <section>
+          <Editor id="1" code="{/* write some code */}" stacked={stackedView} />
+        </section>
+      </div>
     </div>
   );
 };
