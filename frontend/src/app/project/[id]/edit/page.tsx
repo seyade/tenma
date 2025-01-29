5200b5c100e539bc4e08f1800f64dc6d66874526
seyade <sey.adegun@gmail.com>
2025-01-29 22:17:03 +0000

    added icons to buttons and layout general tweaks

--- frontend/src/app/project/[id]/edit/page.tsx
+++ frontend/src/app/project/[id]/edit/page.tsx
@@ -3,6 +3,15 @@
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import Editor from "@/components/Sandbox";
+import {
+  ArrowRight,
+  Columns2,
+  House,
+  LayoutPanelLeft,
+  MoveRight,
+  Rows2,
+} from "lucide-react";
+import Link from "next/link";
 
 // const Editor = dynamic(() => import("@/components/Sandbox"), { ssr: false });
 
@@ -13,6 +22,7 @@ const ProjectEditor = (props: Props) => {
   const [stackedView, setStackedView] = useState(false);
 
   const handleSwitchView = () => {
+    // TODO: persist pane position for user
     setStackedView(!stackedView);
   };
 
@@ -22,7 +32,15 @@ const ProjectEditor = (props: Props) => {
 
   return (
     <div className="plusjakartasans flex">
-      <aside className="bg-white p-1">Sidebar</aside>
+      <aside className="bg-white p-4">
+        <Link href="/">
+          <House />
+        </Link>
+
+        <Link href="/project/1">
+          <LayoutPanelLeft />
+        </Link>
+      </aside>
       <div className="w-full py-3  px-4">
         <header className="flex justify-between mb-6">
           <h1 className="spacegrotesk flex-1 text-3xl font-semibold">
@@ -31,17 +49,23 @@ const ProjectEditor = (props: Props) => {
 
           <div className="flex-1 text-right">
             <button
-              className="py-2 px-4 mr-4 bg-green-600 text-white text-xs rounded-full"
+              className="inline-flex items-center py-2 pl-4 pr-2 mr-4 bg-green-700 text-white text-sm rounded-full"
               onClick={handleSave}
             >
               Save
+              <ArrowRight className="ml-2 text-slate-950 bg-slate-200 rounded-full p-1" />
             </button>
 
             <button
-              className="py-2 px-4 bg-slate-900 text-white text-xs rounded-full"
+              className="inline-flex items-center py-2 pl-4 pr-2 bg-slate-900 text-white text-xs rounded-full"
               onClick={handleSwitchView}
             >
               Change pane View
+              {stackedView ? (
+                <Rows2 className="ml-2 text-slate-950 bg-slate-200 rounded-full p-1" />
+              ) : (
+                <Columns2 className="ml-2 text-slate-950 bg-slate-200 rounded-full p-1" />
+              )}
             </button>
           </div>
         </header>
--- frontend/src/app/project/[id]/page.tsx
+++ frontend/src/app/project/[id]/page.tsx
@@ -1,9 +1,24 @@
+import { Pencil } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const Project = (props: Props) => {
-  return <div>Project</div>;
+  return (
+    <div className="plusjakartasans">
+      <header className="flex justify-between mb-6">
+        <h1 className="spacegrotesk flex-1 text-3xl font-semibold">Project</h1>
+        <Link
+          className="inline-flex items-center py- pl-4 pr-2 mr-4 bg-green-700 text-white text-sm rounded-full"
+          href="/project/1/edit"
+        >
+          Edit
+          <Pencil className="ml-2 text-slate-950 bg-slate-200 rounded-full p-1" />
+        </Link>
+      </header>
+    </div>
+  );
 };
 
 export default Project;
