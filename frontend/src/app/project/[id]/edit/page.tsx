a44aa8ca7bf62a16ff5ab412476619a6e44a98ef
seyade <sey.adegun@gmail.com>
2025-01-29 21:30:12 +0000

    started editor component

--- frontend/src/app/project/[id]/edit/page.tsx
+++ frontend/src/app/project/[id]/edit/page.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import Editor from "@/components/Sandbox";
+
+// const Editor = dynamic(() => import("@/components/Sandbox"), { ssr: false });
 
 type Props = {};
 
 const ProjectEditor = (props: Props) => {
-  return <div>ProjectEditor</div>;
+  const [code, setCode] = useState<string>("");
+  const [stackedView, setStackedView] = useState(false);
+
+  const handleSwitchView = () => {
+    setStackedView(!stackedView);
+  };
+
+  return (
+    <div className="plusjakartasans py-3 px-4">
+      <header className="flex justify-between mb-6">
+        <h1 className="spacegrotesk text-3xl font-semibold">Edit Project</h1>
+        <button
+          className="py-2 px-4 bg-slate-900 text-white text-xs rounded-full"
+          onClick={handleSwitchView}
+        >
+          Change pane View
+        </button>
+      </header>
+
+      <section>
+        <Editor id="1" code="{/* write some code */}" stacked={stackedView} />
+      </section>
+    </div>
+  );
 };
 
 export default ProjectEditor;
--- frontend/src/components/Sandbox/Sandbox.tsx
+++ frontend/src/components/Sandbox/Sandbox.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
 
-const Sandbox = () => {
-  return <div>Sandbox</div>;
+import { LiveProvider, LiveEditor, LivePreview, LiveError } from "react-live";
+
+type SandboxProps = {
+  id: string;
+  code: string;
+  stacked?: boolean;
+};
+
+const Sandbox = ({ id = "1", code, stacked }: SandboxProps) => {
+  return (
+    <LiveProvider code={code} enableTypeScript>
+      <div className={`flex ${stacked ? "flex-col" : ""} gap-2 h-screen`}>
+        <LiveEditor
+          className="h-full w-full text-sm font-mono bg-[#011627]"
+          tabMode="indentation"
+        />
+        <LivePreview className="h-full w-full bg-white border border-slate-400" />
+        <LiveError className="text-red-800 text-sm p-3 bg-red-200" />
+      </div>
+    </LiveProvider>
+  );
 };
 
 export default Sandbox;
