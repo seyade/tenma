4fdc2cc381693418f4c8ffa641c3272879f633bb
seyade <sey.adegun@gmail.com>
2025-01-30 23:59:42 +0000

    some ui tweaks

--- frontend/src/app/(marketing)/explore/page.tsx
+++ frontend/src/app/(marketing)/explore/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="plusjakartasans grid grid-cols-12 ">
+      <header>
+        <h1 className="spacegrotesk flex-1 text-3xl font-semibold">Explore</h1>
+      </header>
+      <section></section>
+    </div>
+  );
 };
 
 export default Explore;
