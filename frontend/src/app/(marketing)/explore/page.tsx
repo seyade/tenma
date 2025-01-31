62b8e4b1a08915280a7076eca386da60856c9ecd
seyade <sey.adegun@gmail.com>
2025-01-31 00:11:31 +0000

    did a bit of makeup on homepage

--- frontend/src/app/(marketing)/explore/page.tsx
+++ frontend/src/app/(marketing)/explore/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Explore = () => {
   return (
-    <div className="plusjakartasans grid grid-cols-12 ">
+    <div className="plusjakartasans grid grid-cols-12 p-10">
       <header>
-        <h1 className="spacegrotesk flex-1 text-3xl font-semibold">Explore</h1>
+        <h1 className="spacegrotesk flex-1 text-7xl">Explore</h1>
       </header>
       <section></section>
     </div>
--- frontend/src/app/(marketing)/page.tsx
+++ frontend/src/app/(marketing)/page.tsx
@@ -13,19 +13,24 @@ export default function Home() {
           </nav>
           <div className="flex gap-4 font-semibold">
             <Link
-              className="py-2 px-5 bg-amber-500 rounded-full"
+              className="py-2 px-5 bg-[#2e2c28] text-white rounded-full"
               href="/register"
             >
               Join
             </Link>
             <Link
-              className="py-2 px-5 bg-amber-300 rounded-full"
+              className="py-2 px-5 rounded-full border border-[#9e9b96]"
               href="/sign-in"
             >
               Sign in
             </Link>
           </div>
         </header>
+        <section className="p-10">
+          <div className="flex p-10 bg-[#9e9b96]/50 rounded-3xl">
+            <p className="spacegrotesk flex-1 text-8xl">Be seen by others.</p>
+          </div>
+        </section>
       </main>
     </div>
   );
