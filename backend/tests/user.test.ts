61530b2e99bf72dbc4eefe7148eb60724afdcae6
seyade <sey.adegun@gmail.com>
2025-03-08 22:01:23 +0000

    created unit-test for getting users and single user

--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -1,55 +1,65 @@
-import bcrypt from "bcryptjs";
-import { PrismaClient } from "@prisma/client";
-import { mockDeep, DeepMockProxy } from "jest-mock-extended";
-import userResolver from "../src/graphql/resolvers/user";
+import dotenv from "dotenv";
+import request from "supertest";
 
-jest.mock("@prisma/client", () => {
-  const PrismaClientMock = jest.fn(() => ({
-    user: {
-      findMany: jest.fn(),
-      findUnique: jest.fn(),
-      create: jest.fn(),
+dotenv.config();
+
+jest.mock("../src/config", () => ({
+  __esModule: true,
+  config: {
+    PORT: 4000,
+    prisma: {
+      user: {
+        findMany: jest.fn(),
+        findUnique: jest.fn(),
+      },
     },
-  }));
-
-  return { PrismaClient: PrismaClientMock };
-});
-
-jest.mock("bcryptjs", () => ({
-  hash: jest.fn(() => Promise.resolve("hashedPassword")),
+  },
 }));
 
-const prismaMock = mockDeep<PrismaClient>();
+import { app } from "../src/express";
+import { config } from "../src/config";
 
-describe("User Resolver", () => {
-  const mockUser = {
-    id: 1,
-    userId: "c642aec5-3011-47e5-9174-54c574c4c12a",
-    email: "bulma.brief@capsule.co",
-    username: "bulma",
-    password: "password",
-    profileSummary: "Engineer and QA at Capsule Tests",
-    verificationCode: "111000",
-    isVerified: false,
-    name: "Bulma",
-    title: "Lead QA",
-    appTenure: "2 years",
-    skills: ["C++", "Typescript"],
-    clients: ["Amazon", "Netflix"],
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
+type MockUser = {
+  id: number;
+  userId: string;
+  username: string;
+  password: string;
+  email: string;
+};
+
+describe("GET /users", () => {
+  const mockedUsers: MockUser[] = [
+    {
+      id: 1,
+      userId: "291d15fd-00bd-4528",
+      username: "Youneet Testeen",
+      password: "myunreadablepasssword",
+      email: "youneet@testeen.ai",
+    },
+  ];
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  describe("Query", () => {
-    it("should return all users", async () => {
-      const mockUsers = [mockUser];
-      prismaMock.user.findMany.mockResolvedValue(mockUsers);
-      const result = await userResolver.Query.users();
-      expect(result).toEqual(mockUsers);
-    });
+  it("should list all users", async () => {
+    (config.prisma.user.findMany as jest.Mock).mockResolvedValue(mockedUsers);
+    const response = await request(app).get(`${process.env.API_URL}/users`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.users).toEqual(mockedUsers);
+  });
+
+  it("should get a single users", async () => {
+    const mockUser = mockedUsers[0];
+
+    (config.prisma.user.findUnique as jest.Mock).mockResolvedValue(mockUser);
+
+    const response = await request(app).get(
+      `${process.env.API_URL}/users/${mockUser.userId}`
+    );
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.user).toEqual(mockUser);
   });
 });
